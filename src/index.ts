49ce931b71856e6192b65d1e1b39fb134c27bace
PhaserEditor2D <developers@phasereditor2d.com>
2023-11-30 12:24:29 -0500

    Adds missing easecomp.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as DelayComp } from "./DelayComp";
 export { default as DurationComp } from "./DurationComp";
+export { default as EaseComp } from "./EaseComp";
 export { default as MoveInSceneActionScript } from "./MoveInSceneActionScript";
 export { default as MoveOutSceneActionScript } from "./MoveOutSceneActionScript";
 export { default as PushActionScript } from "./PushActionScript";
