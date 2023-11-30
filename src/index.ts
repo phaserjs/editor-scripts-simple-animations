8c319a862ee91349195ecbe28c8fd1c7f818b39f
PhaserEditor2D <developers@phasereditor2d.com>
2023-11-30 12:21:04 -0500

    Adds missing export.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as DurationComp } from "./DurationComp";
 export { default as MoveInSceneActionScript } from "./MoveInSceneActionScript";
 export { default as MoveOutSceneActionScript } from "./MoveOutSceneActionScript";
 export { default as PushActionScript } from "./PushActionScript";
+export { default as FadeActionScript } from "./FadeActionScript";
