b53df7f2ccc3afcca4110421c58aebbac489f67b
PhaserEditor2D <developers@phasereditor2d.com>
2023-12-01 09:43:07 -0500

    Install script code from npm.

--- install-local.sh
+++
@@ -1,2 +0,0 @@
-rm -Rf node_modules package-lock.json
-npm install --registry http://localhost:4873/
--- install-prod.sh
+++
@@ -1,2 +0,0 @@
-rm -Rf node_modules package-lock.json
-npm install --registry https://registry.npmjs.org
